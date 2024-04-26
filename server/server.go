07b98883c0908818d8f24cee4e3890e3f710ff15
KarinaRdk <101858179+KarinaRdk@users.noreply.github.com>
2024-04-26 20:39:53 +0300

    Add files via upload

---
+++ server/server.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Server represents the HTTP server.
+type Server struct {
+	httpServer *http.Server
+}
+
+// NewServer creates a new server instance.
+func New(address string, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    address,
+			Handler: handler,
+		},
+	}
+}
+
+// Start starts the server.
+func (s *Server) Start() {
+	go func() {
+		if err := s.httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+		log.Println("Server Started")
+	}()
+}
+
+// Stop gracefully shuts down the server.
+func (s *Server) Stop() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		log.Fatalf("Server failed to shut down gracefully: %v", err)
+	}
+}
